660811c45ff41beaaf585719f3ea33b40cca8b97
Gabriel <gabriel.luthun@gmail.com>
2024-07-29 12:19:03 +0200

    feat: add file for interaction with db

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+
+namespace APIContacts.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+       
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+
+   
+}
+ 
